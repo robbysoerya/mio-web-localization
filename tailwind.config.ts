9894f0c0aac6afa66f179ac72033119c996ae69a
Robby surya Pratama <robbysoerya6@gmail.com>
2025-12-07 08:02:07 +0700

    refactor: change tailwind config from js to typescript

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+import tailwindcssAnimate from "tailwindcss-animate";
+
+const config: Config = {
     darkMode: ["class"],
     content: [
     "./app/**/*.{js,ts,jsx,tsx}",
@@ -56,5 +58,7 @@ module.exports = {
   		}
   	}
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
+
+export default config;
